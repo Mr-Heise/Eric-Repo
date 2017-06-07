43f9b8b0d22af095a93ab87fc8c52fe4d1c88613
Eric <en265@gssdschools.ca>
2017-06-07 10:58:24 -0600

    Add files via upload

---
+++ ParseString.java
@@ -0,0 +1,18 @@
+/*
+ *ParseString.java
+ *Eric
+ *CSC 30
+ *Start date june 7 2017
+ *This program gets stuff
+ */
+public class ParseString 
+{
+	public static void main(String[] args)
+	{
+		
+		String lineText = "Babyak,Akilah,2000,6,June";
+		
+		System.out.println("\n\t**** Word stuff ****");
+		
+	}
+}
